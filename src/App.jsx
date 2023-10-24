0611ccfa50b4d516e36a9debe003e626635bb019
riczudaniel <you@example.com>
2023-10-24 08:55:16 +0200

    Tab Buttons jsx with props.children

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from "./data"
 import Header from "./components/Header/Header.jsx"
 import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 
 
@@ -20,6 +21,17 @@ function App() {
         </ul>
         </section>
         <h2>Time to get started!</h2>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
+
       </main>
     </div>
   );
