6401daadb90872c37e02c9d2a35cc22fa7b47daa
riczudaniel <you@example.com>
2023-10-25 06:59:08 +0200

    <div id tab-content> wad added with data fetched from data.js, updated with State

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react"
 
 import { CORE_CONCEPTS } from "./data"
+import { EXAMPLES } from "./data"
+
 import Header from "./components/Header/Header.jsx"
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 
 
+
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState("Please click a button")
+  const [ selectedTopic, setSelectedTopic ] = useState("components")
 
 
 
@@ -40,14 +43,23 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect ("Component")}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect ("JSX") }>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect ("Props") }>Props</TabButton>
-            <TabButton onSelect={() => handleSelect ("State") }>State</TabButton>
+            <TabButton onSelect={() => handleSelect ("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect ("jsx") }>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect ("props") }>Props</TabButton>
+            <TabButton onSelect={() => handleSelect ("state") }>State</TabButton>
           </menu>
 
           {/* This content will be updated, depending on what we click. */}
           <h2>{selectedTopic}</h2>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+               {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
 
       </main>
